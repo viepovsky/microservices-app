3c5f494538f8d5f7aaef4d89b144cef88d298057
Oskar R <o.rajzner@gmail.com>
2023-11-15 11:01:29 +0100

    Project starter with 1 module(service)

---
+++ barber/src/main/java/com/viepovsky/barber/BarberApplication.java
@@ -0,0 +1,11 @@
+package com.viepovsky.barber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BarberApplication.class, args);
+    }
+}
