ec5d63667b88ce1ba17bf9660bcb4d6bc611bb50
Oskar R <o.rajzner@gmail.com>
2023-11-15 21:02:45 +0100

    Add 2nd microservice, mysql database and entity class

---
+++ recommendation/src/main/java/com/viepovsky/recommendation/Recommendation.java
@@ -0,0 +1,31 @@
+package com.viepovsky.recommendation;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+class Recommendation {
+    @Id
+    @SequenceGenerator(
+            name = "recommendation_id_sequence",
+            sequenceName = "recommendation_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "recommendation_id_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long id;
+    private Long barberId;
+    private boolean isRecommended;
+    private LocalDateTime recommendationTime;
+}
