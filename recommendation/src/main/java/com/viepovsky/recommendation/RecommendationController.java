3df1cf9adee4c7be5f5eb9196498cae52aea4056
Oskar R <o.rajzner@gmail.com>
2023-11-16 08:55:05 +0100

    Add new module with eureka server, add basic configuration

--- recommendation/src/main/java/com/viepovsky/recommendation/RecommendationController.java
+++ recommendation/src/main/java/com/viepovsky/recommendation/RecommendationController.java
@@ -1,7 +1,9 @@
 package com.viepovsky.recommendation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/recommendations")
 class RecommendationController {
@@ -14,6 +16,7 @@ class RecommendationController {
     @GetMapping("/{barberId}")
     RecommendationCheckResponse isBarberRecommended(@PathVariable Long barberId) {
         boolean isRecommended = service.isBarberRecommended(barberId);
+        log.info("New recommendation check for barber {}", barberId);
         return new RecommendationCheckResponse(isRecommended);
     }
 }
