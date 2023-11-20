8e6bddd3144202075e5796a90365bea21ad58dd0
Oskar R <o.rajzner@gmail.com>
2023-11-20 13:24:53 +0100

    Add rabbitmq dependencies, set up rabbitmq connection

--- notification/src/main/java/com/viepovsky/notification/NotificationApplication.java
+++ notification/src/main/java/com/viepovsky/notification/NotificationApplication.java
@@ -3,9 +3,22 @@ package com.viepovsky.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.viepovsky.notification",
+        "com.viepovsky.amqp"
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+// Uncomment to check connection with the RabbitMQ
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//        return args -> {
+//                    producer.publish("foo",
+//                            notificationConfig.getInternalExchange(),
+//                            notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
 }
